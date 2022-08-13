a0703d8eb704cb06d4969b0267ee1c2bfe8769f0
Manu Anish <manusanish@gmail.com>
2022-08-13 12:45:22 +0530

    Syntax Highlighting

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,15 @@
 
 import withMDX from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import remarkPrism from "remark-prism";
 
 const NextConfig = withMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [],
+    remarkPlugins: [remarkFrontmatter, remarkMath, remarkPrism],
+    rehypePlugins: [rehypeKatex],
   },
 });
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import "inter-ui/inter.css";
 import "@fontsource/urbanist/400.css";
 import "@fontsource/urbanist/500.css";
@@ -6,6 +5,9 @@ import "@fontsource/urbanist/600.css";
 import "@fontsource/urbanist/700.css";
 import "@fontsource/urbanist/800.css";
 import "@fontsource/urbanist/900.css";
+import "katex/dist/katex.min.css";
+import "@docs/prism/themes/default.css";
+import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
