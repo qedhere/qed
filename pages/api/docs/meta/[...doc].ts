28e5de3a23fb94494a43f5e7e4e6a63c113372ef
Manu Anish <manusanish@gmail.com>
2022-08-31 12:22:47 +0530

    Fixing Dynamic API Routes

--- pages/api/docs/meta/[...doc].ts
+++ pages/api/docs/meta/[...doc].ts
@@ -3,7 +3,7 @@ import error from "@api/error_codes.json";
 import { parseFrontmatter } from "@lib/parseFrontmatter.js";
 
 const MetaDoc = (req: NextApiRequest, res: NextApiResponse) => {
-  res.send(req)
+  res.send(req.query.doc)
 };
 
 export default MetaDoc;
