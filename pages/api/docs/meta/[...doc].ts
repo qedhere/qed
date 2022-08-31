ec55f02471eef454259e6d442d211dac768c9886
Manu Anish <manusanish@gmail.com>
2022-08-31 12:14:04 +0530

    Remove Typecheck (debug)

--- pages/api/docs/meta/[...doc].ts
+++ pages/api/docs/meta/[...doc].ts
@@ -3,7 +3,7 @@ import error from "@api/error_codes.json";
 import { parseFrontmatter } from "@lib/parseFrontmatter.js";
 
 const MetaDoc = (req: NextApiRequest, res: NextApiResponse) => {
-  if (Array(req.query.doc!).length == 1) {
+  if (req.query.doc!.length == 1) {
     try {
       const matter = JSON.stringify(
         parseFrontmatter("/docs/" + req.query.doc![0]),
@@ -19,7 +19,7 @@ const MetaDoc = (req: NextApiRequest, res: NextApiResponse) => {
     } catch {
       res.send(error.ERR_INVALID_ROUTE);
     }
-  } else if (Array(req.query.doc!).length == 2) {
+  } else if (req.query.doc!.length == 2) {
     try {
       const matter = JSON.stringify(
         parseFrontmatter(
@@ -37,7 +37,7 @@ const MetaDoc = (req: NextApiRequest, res: NextApiResponse) => {
     } catch {
       res.send(error.ERR_INVALID_ROUTE);
     }
-  } else if (Array(req.query.doc!).length == 3) {
+  } else if (req.query.doc!.length == 3) {
     try {
       const matter = JSON.stringify(
         parseFrontmatter(
